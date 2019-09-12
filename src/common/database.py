762459376d242c6b24520111d255c88b0e7c3e42
Akashramsankar <44971212+Akashramsankar@users.noreply.github.com>
2019-09-12 14:39:29 +0530

    Update database.py

--- src/common/database.py
+++ src/common/database.py
@@ -2,40 +2,28 @@ import pymongo
 import os
 
 class Database(object):
-
-   # URI = os.environ['MONGODB_URI']
-   # DATABASE = None
-
-   # @staticmethod
-
-   # def initialize():
-
-   #    client = pymongo.MongoClient(Database.URI)
-   #     Database.DATABASE = client['heroku_thg5d5x0']
-
-    URI = "mongodb://127.0.0.1:27017"
+    URI = os.environ['MONGODB_URI']
     DATABASE = None
-
     @staticmethod
     def initialize():
-          client = pymongo.MongoClient(Database.URI)
-          Database.DATABASE = client['Tiruvallur']
+      client = pymongo.MongoClient(Database.URI)
+      Database.DATABASE = client['heroku_vcrk1vkq']
+    # URI = "mongodb://127.0.0.1:27017"
+    # DATABASE = None
+    # @staticmethod
+    # def initialize():
+    #       client = pymongo.MongoClient(Database.URI)
+    #       Database.DATABASE = client['Tiruvallur']
 
     @staticmethod
     def insert(collection, data):
-
         Database.DATABASE[collection].insert(data)
-
     @staticmethod
     def find(collection, query):
-
         return Database.DATABASE[collection].find(query)
-
     @staticmethod
     def find_one(collection, query):
-
         return Database.DATABASE[collection].find_one(query)
-
     @staticmethod
     def Update_Plantation(collection, query,  block, typeOfPlantation, workName, typeOfCrop, totalPits, hectre, panchayat, overseer, habitation,totalSanctionedPlants, pitsTaken, pitsToBeTaken,plotNo,survivalRateOfCrops, user_name, user_id, costOfCrops, plantationDate, plantationStatus):
 
